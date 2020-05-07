d042fd47f6b64b9654a87a45a23680dd1e66b627
Vu Tuan Hoang <hoangvutuan@gmail.com>
2020-05-07 06:25:55 -0700

    test1

---
+++ LeetcodeCSharpV3/LeetcodeCSharpV3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeetcodeCSharpV3
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World 1!");
+        }
+    }
+}
